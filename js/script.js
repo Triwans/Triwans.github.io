3872ee6c523edf4673b2c1bfecb03d4234dbf399
Triwans <86065183+Triwans@users.noreply.github.com>
2021-06-19 00:57:37 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const txtElement = ['Freelancer', 'Web Developer', 'Graphic Designer'];
+const txtElement = ['Freelancer', 'Web Developer', 'Mahasiswa'];
 let count = 0;
 let txtIndex = 0;
 let currentTxt = '';
